7913db1ef3ff1dcc801970f9e5ea055a608de846
MateusProDev <mateusferreiraprodev@gmail.com>
2024-03-04 23:36:22 -0300

    add

--- js/utils.js
+++ js/utils.js
@@ -64,7 +64,7 @@ function showUserContent(user) {
     }
   }
   
-  userImg.src = user.photoURL ? user.photoURL : ''
+  userImg.src = user.photoURL ? user.photoURL : 'img/placeholder.png'
   userName.innerHTML = user.displayName
   userEmail.innerHTML = user.email
   hideItem(auth)
